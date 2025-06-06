c7c45a0778ca3fb8ec713b3ad1208b5c444568ab
Kopylov Denis <kopyloveden@gmail.com>
2025-06-06 20:52:44 +0300

    feat: add container endpoints with service logic

--- src/api/v1/container_endpoint.py
+++ src/api/v1/container_endpoint.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter
-import docker 
+
+from src.services.docker_service import get_containers, get_container
 
 
 router = APIRouter(prefix="/containers")
 
-docker_client = docker.from_env()
-
 @router.get("")
-async def get_containers():
-    pass 
+async def get_containers_endpoint(all: bool = False):
+    return get_containers(all)
     
+    
+@router.get("/{container_id}")
+async def get_container_endpoint(container_id: str):
+    return get_container(container_id)
---
+++ src/services/docker_service.py
@@ -0,0 +1,28 @@
+from src.integrations.docker_client import docker_client
+from docker.models.containers import Container
+from docker.errors import NotFound
+from fastapi import HTTPException, status
+
+
+def serialize_container(container: Container) -> dict:
+    return {
+        "id": container.id,
+        "image": container.image.tags if container.image.tags else container.image.short_id,
+        "status": container.status,
+        "ports": container.ports,
+        "name": container.name,
+    }
+
+
+def get_containers(all: bool = False) -> list[dict]:
+    containers: list[Container] = docker_client.containers.list(all=all)
+    return [serialize_container(container) for container in containers]
+
+
+def get_container(container_id: str) -> dict:
+    try:
+        container = docker_client.containers.get(container_id)
+    except NotFound as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="container not found")
+    
+    return serialize_container(container)
